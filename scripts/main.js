e2c5a229e6a5a0b7a94bf2f6db12c57de8011354
marpot <marcin.potoczny@protonmail.com>
2022-04-19 15:37:23 +0200

    Changes

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
-var words = ['Hello', 'World'];
-for (var _i = 0, words_1 = words; _i < words_1.length; _i++) {
-    var word = words_1[_i];
+"use strict";
+let words = ['Hello', 'World'];
+for (let word of words) {
     console.log(word);
 }
